f051fb205d584a5adf318db08ad7c073d13bc761
Vinay <vinaysinghvsk8803@gmail.com>
2021-12-16 16:09:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-
+import { useState } from "react";
+import Alert from "./components/Alert";
+// import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setalert] = useState(null);
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setalert(null);
+    }, 2000);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode enable", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode enable", "success");
+    }
+  };
   return (
     <>
-      <Navbar tittle="TextUtils" About="About US" />
+    {/* <Router> */}
+      <Navbar tittle="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter your Text here" />
-        {/* <About/> */}
+      {/* <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          
+          <Route exact path="/"> */}
+          <TextForm
+          showAlert={showAlert}
+          heading="Enter your Text here"
+          mode={mode}
+        />
+          {/* </Route>
+        </Switch> */}
+
       </div>
+       {/* </Router> */}
+     
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,64 +1,86 @@
-import React , {useState} from "react";
-
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleClick = () => {
+    // console.log("this was click" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to upppercase","primary");
+  };
+  const handleLowClick = () => {
+    // console.log("this was click" + text);
+    let newText = text.toLowerCase();
+    props.showAlert("Converted to Lowercase","primary");
 
-    const handleClick = ()=>{
-        // console.log("this was click" + text);
-        let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleClearClick = () => {
+    // console.log("this was click" + text);
+    let newText = "";
 
-        setText(newText);
-    }
-    const handleLowClick = ()=>{
-        // console.log("this was click" + text);
-        let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Text Box has been reset","success");
+  };
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
 
-        setText(newText);
-    }
-    const handleClearClick = ()=>{
-        // console.log("this was click" + text);
-        let newText = '';
+    setText(newText.join(" "));
+    props.showAlert("Extra Spaces has been Deleted","success");
+  };
 
-        setText(newText);
-    }
-    const handleExtraSpaces = ()=>{
-        let newText = text.split(/[ ]+/);
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
 
-        setText(newText.join(" "));
-    }
-
-
-    const handleOnChange = (event)=>{
-        setText(event.target.value);
-    }
-    const [text,setText] = useState('');
-  
-  
-    return (
-        <>
-            <div className="container">
-            <h2>{props.heading}</h2>
-            <div className="mb-3">
-
-                <textarea
-                    className="form-control"
-                    value={text} onChange={handleOnChange}
-                    id="myBox"
-                    rows="8"
-                ></textarea>
-            </div>
-            <button className="btn btn-primary mx-2" onClick = {handleClick}>Convert to UpperCase</button>
-            <button className="btn btn-primary mx-2" onClick = {handleLowClick}>Convert to LowerCase</button>
-            <button className="btn btn-primary mx-2" onClick = {handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-2" onClick = {handleExtraSpaces}>Remove Extra Spaces</button>
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "black"
+        }}
+      >
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            
+            style={{
+                backgroundColor: props.mode === "dark" ? "#042743" : "white",
+                color: props.mode === "dark" ? "white" : "#042743",
+              }}
+            id="myBox"
+            rows="8"
+          ></textarea>
         </div>
-        <div className="container my-4">
-                <h2>Your text Summary</h2>
-                <p>{text.split(" ").length} words, {text.length} characters</p>
-                <p>{0.008 *text.split(" ").length} Minutes for read</p>
-                <h2>Preview</h2>
-                <p>{text}</p>
-        </div>
-        </>
-    );
+        <button className="btn btn-primary mx-2" onClick={handleClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
+        </button>
+      </div>
+      <div
+        className="container my-4"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h2>Your text Summary</h2>
+        <p>
+          {text.split(" ").length} words, {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes for read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter Something in textbox above"}</p>
+      </div>
+    </>
+  );
 }
